9980c1fc59b3aa04a4f7ba0a71044ec0e79e8c2e
SeregaT79 <skr-on@mail.ru>
2020-04-11 01:39:41 +0300

    в бане веник дороже денег

--- lesson_002/07_secret.py
+++ lesson_002/07_secret.py
@@ -20,5 +20,10 @@ secret_message = [
 #
 # Требуется задать конкретные индексы, например secret_message[3][12:23:4]
 # Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
-
-# TODO вывести расшифрованное сообщение
+# massege =
+word_1 = secret_message[0][3]
+word_2 = secret_message[1][9:13]
+word_3 = secret_message[2][5:15:2]
+word_4 = secret_message[3][12:6:-1]
+word_5 = secret_message[4][20:15:-1]
+print(word_1 + ' ' + word_2+ ' ' + word_3 + ' ' + word_4 + ' ' + word_5)
