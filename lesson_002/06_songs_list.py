648c6cac4c000bb944ebbb7a8c255280a9f37f7b
SeregaT79 <skr-on@mail.ru>
2020-04-11 01:09:44 +0300

    song sum

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -19,8 +19,8 @@ violator_songs_list = [
 #   Три песни звучат ХХХ минут
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
-
-# TODO здесь ваш код
+long = violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[-1][1]
+print(long)
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -38,4 +38,5 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+song = violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress']
+print(song)
