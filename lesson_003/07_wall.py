ab4f065cce9a52156b3c5d013c31c6e29759d41d
SeregaT79 <skr-on@mail.ru>
2020-04-13 21:32:15 +0300

    построил стену

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+sd.resolution = (1100, 600)
+ix = 0
+jy = 0
 
-simple_draw.pause()
+left_bottom = sd.get_point(0, 0)
+right_top = sd.get_point(100, 50)
+
+
+for i in range(0, 700, 50):
+    for k in range(0, 1300, 100 ):
+        sd.rectangle(sd.get_point(ix, jy), sd.get_point(ix+k, jy+i), width=2)
+
+
+sd.pause()
