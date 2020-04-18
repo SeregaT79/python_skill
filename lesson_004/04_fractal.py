e535ca70784ca23e6462518bec6cdbffa1e67815
SeregaT79 <skr-on@mail.ru>
2020-04-19 02:29:35 +0300

    херня какая-то

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+import random
 
 sd.resolution = (800, 600)
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
@@ -12,6 +13,7 @@ sd.resolution = (800, 600)
 
 point_0 = sd.get_point(300, 300)
 
+
 def draw_branches(point=[0, 0], angle=[0, 0], length=[100, 100], delta=0):
     if length[0] < 1:
         return
@@ -20,24 +22,38 @@ def draw_branches(point=[0, 0], angle=[0, 0], length=[100, 100], delta=0):
     v1.draw()
     v2.draw()
     next_angle = [angle[0] - delta, angle[1] + delta]
-    next_length = [length[0]*0.75, length[1]*0.75]
-    next_point =[v1.end_point, v2.end_point]
+    next_length = [length[0] * 0.75, length[1] * 0.75]
+    next_point = [v1.end_point, v2.end_point]
     draw_branches(point=next_point, angle=next_angle, length=next_length, delta=delta)
 
 
-
-draw_branches(point=[point_0, point_0], angle=[90, 105], length=[50, 40], delta=25)
+# draw_branches(point=[point_0, point_0], angle=[90, 105], length=[50, 40], delta=25)
 
 
 # 2) Сделать draw_branches рекурсивной
 # - добавить проверку на длину ветвей, если длина меньше 10 - не рисовать
 # - вызывать саму себя 2 раза из точек-концов нарисованных ветвей,
 #   с параметром "угол рисования" равным углу только что нарисованной ветви,
-#   и параметром "длинна ветвей" в 0.75 меньшей чем длина только что нарисованной ветви
+# def draw_bunches(start_point=0, angle=90, length=100):
+#     if length < 10:
+#         return
+#     v1 = sd.get_vector(start_point=start_point, angle=angle, length=length)
+#     v1.draw()
+#     next_length = length * 0.75
+#     next_point = v1.end_point
+#     next_angle = angle
+#     n=0
+#     while n < 2:
+#         draw_bunches(start_point=next_point, angle=angle, length=next_length)
+#         angle+=10
+#         n+=1
+# #
+#
+# # 3) первоначальный вызов:
+# root_point = sd.get_point(300, 30)
+# for i in range(5, 25, 5):
+#     draw_bunches(start_point=root_point, angle=90+i, length=150-i)
 
-# 3) первоначальный вызов:
-# root_point = get_point(300, 30)
-# draw_bunches(start_point=root_point, angle=90, length=100)
 
 # Пригодятся функции
 # sd.get_point()
@@ -46,7 +62,24 @@ draw_branches(point=[point_0, point_0], angle=[90, 105], length=[50, 40], delta=
 
 # можно поиграть -шрифтами- цветами и углами отклонения
 
-# TODO здесь ваш код
+
+root_point = sd.get_point(400, 30)
+
+
+def draw_bunches(point, angle, length, delta):
+    if length < 1:
+        return
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length)
+    v1.draw()
+    next_point = v1.end_point
+    next_angle = random.randint(delta, 180)
+    next_length = length * 0.75
+    draw_bunches(point=next_point, angle=next_angle, length=next_length, delta=delta)
+
+
+for i in range(5, 50, 10):
+    draw_bunches(point=root_point, angle=120, length=200, delta=i)
+
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
@@ -57,5 +90,3 @@ draw_branches(point=[point_0, point_0], angle=[90, 105], length=[50, 40], delta=
 # sd.random_number()
 
 sd.pause()
-
-
