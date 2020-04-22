22d5ba76a36eb1fb851765a2b8ca9df0eba590a7
SeregaT79 <skr-on@mail.ru>
2020-04-22 22:03:02 +0300

    соседи есть

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -7,5 +7,10 @@ import room_1
 
 print('В комнате room_1 живут:')
 print(room_1.folks[0], 'и', room_1.folks[1])
+print()
 
+import room_2
+
+print('В комнате room_2 живет:')
+print(room_2.folks[0])
 
