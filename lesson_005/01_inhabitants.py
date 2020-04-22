3db358b46c52ef592786693611134247b43464a9
SeregaT79 <skr-on@mail.ru>
2020-04-22 21:20:57 +0300

    Next

---
+++ lesson_005/01_inhabitants.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Вывести на консоль жителей комнат (модули room_1 и room_2)
+# Формат: В комнате room_1 живут: ...
+
+import room_1
+
+print('В комнате room_1 живут:')
+print(room_1.folks[0], 'и', room_1.folks[1])
+
+
