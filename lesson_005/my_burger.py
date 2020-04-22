ed0d717c90b1c291869d8b50e3de544b33133050
SeregaT79 <skr-on@mail.ru>
2020-04-22 22:43:49 +0300

    создал файл

---
+++ lesson_005/my_burger.py
@@ -0,0 +1,17 @@
+def bulki():
+    print('А теперь добавим булки')
+
+def kotleta():
+    print("Жарим котлеты")
+
+def ogurec():
+    print('А теперь добавим огурец')
+
+def tomato():
+    print('А теперь добавим помидорчик')
+
+def maionez():
+    print('А теперь добавим майонез')
+
+def cheez():
+    print('А теперь добавим сыр')
