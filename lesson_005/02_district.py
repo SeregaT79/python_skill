6d80bec35c21f433ff1bf9645f3f3df74403b65d
SeregaT79 <skr-on@mail.ru>
2020-04-22 22:17:28 +0300

    все жильцы определены

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -4,10 +4,17 @@
 # Формат вывода: На районе живут ...
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
-import room_1
 
+from district.central_street.house1 import  room1, room2
 
+prob = ', '
+print('На районе в первом доме живут:')
+print(prob.join(room1.folks))
+print(prob.join(room2.folks))
+print()
 
+from district.central_street.house2 import room1, room2
 
-
+print('На районе во втором доме живут:')
+print(prob.join(room2.folks))
 
