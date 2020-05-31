cdb9ed3b0ca46849f826e68d4d89d17b282af952
SeregaT79 <skr-on@mail.ru>
2020-05-31 16:33:04 +0300

    Вася будет жить

--- lesson_007/python_snippets/my_practice.py
+++ lesson_007/python_snippets/my_practice.py
@@ -1,3 +1,7 @@
+from random import randint
+from termcolor import cprint
+
+
 class Man:
 
     def __init__(self, name):
@@ -10,25 +14,48 @@ class Man:
         return f"Я - {self.name}, сытость {self.fullness}, еды осталось {self.food}, денег осталось {self.money}"
 
     def eat(self):
-        if self.food > 10:
-            print(f'{self.name} поел')
-            self.fullness += 10 
+        if self.food >= 10:
+            cprint(f'{self.name} поел', color='yellow')
+            self.fullness += 10
             self.food -= 10
         else:
             print(f'{self.name} нет еды')
 
     def work(self):
-        print(f'{self.name} сходил на работу')
+        cprint(f'{self.name} сходил на работу', color='red')
         self.money += 50
         self.fullness -= 10
 
     def play_DOTA(self):
-        print(f'{self.name} играл в DOTA')
+        cprint(f'{self.name} играл в DOTA', color='green')
         self.fullness -= 10
 
+    def shopping(self):
+        if self.money >= 50:
+            print(f'{self.name} сходил в магазин и купил продуктов')
+            self.food += 50
+            self.money -= 50
+        else:
+            print(f'{self.name} деньги кончились')
 
-man = Man(name='Вася')
-print(man)
-man.eat()
-print(man)
-man.work()
+    def act(self):
+        if self.fullness <= 0:
+            cprint(f'{self.name} умер', color='cayen')
+            return
+        
+        if self.fullness < 20:
+            self.eat()
+        elif self.food < 10:
+            self.shopping()
+        elif self.money < 50:
+            self.work() 
+        else:
+            self.play_DOTA()        
+
+
+vasya = Man(name='Вася')
+
+for day in range(1, 21):
+    print(f'\n==================== день {day} ==========================\n')
+    vasya.act()
+    print(vasya)
